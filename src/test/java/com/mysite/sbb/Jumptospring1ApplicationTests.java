996afe743ca58a305c517c149fcdf7baf24279ae
pcyscott <pcyscott@gmail.com>
2024-12-19 10:24:21 +0900

    spring2-4

---
+++ src/test/java/com/mysite/sbb/Jumptospring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jumptospring1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
