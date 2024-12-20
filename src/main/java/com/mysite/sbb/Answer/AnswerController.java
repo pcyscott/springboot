c7552c809fdefa87b65fe33cfb75b077078b92bf
pcyscott <pcyscott@gmail.com>
2024-12-20 14:18:01 +0900

    adding answer

--- src/main/java/com/mysite/sbb/Answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/Answer/AnswerController.java
@@ -1,4 +1,26 @@
 package com.mysite.sbb.Answer;
 
+import com.mysite.sbb.Question.Question;
+import com.mysite.sbb.Question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
 public class AnswerController {
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Long id, @RequestParam(value="content") String content){
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question,content);
+        return String.format("redirect://question/details/%s", id);
+    }
 }
