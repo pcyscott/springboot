8abc7af975c49e36fc801639218645d0cde00813
pcyscott <pcyscott@gmail.com>
2024-12-20 10:47:47 +0900

    making details

--- src/main/java/com/mysite/sbb/Question/QuestionService.java
+++ src/main/java/com/mysite/sbb/Question/QuestionService.java
@@ -2,15 +2,24 @@ package com.mysite.sbb.Question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import com.mysite.sbb.DataNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
-
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+    public Question getQuestion(Long id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }
+        else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
