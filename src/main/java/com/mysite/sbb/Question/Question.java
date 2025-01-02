3d1acbf5d4c4ab8d375d36eed17f615d4838e8aa
pcyscott <pcyscott@gmail.com>
2025-01-02 15:48:10 +0900

    add author at table

--- src/main/java/com/mysite/sbb/Question/Question.java
+++ src/main/java/com/mysite/sbb/Question/Question.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.Question;
 
 import com.mysite.sbb.Answer.Answer;
+import com.mysite.sbb.User.SiteUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,4 +27,7 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
+
+    @ManyToOne
+    private SiteUser author;
 }
