dd884fd93d719fc42cf476a5e2ae48e2897a1184
pcyscott <pcyscott@gmail.com>
2025-01-06 10:08:48 +0900

    adding anker function

--- src/main/java/com/mysite/sbb/Answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/Answer/AnswerService.java
@@ -15,13 +15,14 @@ import java.util.Optional;
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser author) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateTime(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+        return answer;
     }
 
     public Answer getAnswer(Long id) {
@@ -40,5 +41,10 @@ public class AnswerService {
 
     public void delete(Answer answer) {
         this.answerRepository.delete(answer);
+    }
+
+    public void vote(Answer answer, SiteUser siteUser){
+        answer.getVoter().add(siteUser);
+        this.answerRepository.save(answer);
     }
 }
